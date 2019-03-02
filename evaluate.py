a5c8fabc749370c5263e227102e001f19aaaf266
Naoki Otani <notani@cs.cmu.edu>
2019-03-01 22:56:24 -0500

    Set the default max vocabulary size to -1 (unlimited)

--- evaluate.py
+++ evaluate.py
@@ -30,7 +30,7 @@ parser.add_argument("--dico_eval", type=str, default="default", help="Path to ev
 # reload pre-trained embeddings
 parser.add_argument("--src_emb", type=str, default="", help="Reload source embeddings")
 parser.add_argument("--tgt_emb", type=str, default="", help="Reload target embeddings")
-parser.add_argument("--max_vocab", type=int, default=200000, help="Maximum vocabulary size (-1 to disable)")
+parser.add_argument("--max_vocab", type=int, default=-1, help="Maximum vocabulary size (-1 to disable)")
 parser.add_argument("--emb_dim", type=int, default=300, help="Embedding dimension")
 parser.add_argument("--normalize_embeddings", type=str, default="", help="Normalize embeddings before training")
 parser.add_argument("--drop_mwe", action="store_true", help="Drop mwes combined with _ from vocabularies")
